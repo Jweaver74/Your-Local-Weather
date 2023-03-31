47518628c9bd47564c87397a5e38e64b0d73e6f0
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-03-31 10:46:56 -0500

    added date element and image

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+var cities =[];
+var cityFormEl = document.querySelector("#city-search-form");
+var cityInputEl = document.querySelector("#city");
+var weatherContainerEl = document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#five-day-container");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if (city) {
+        getCityWeather(city);
+        get5day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    }else{
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
+
+var saveSearch = function() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+// display weather to page
+var getCityWeather = function(city) {
+    var apiKey ="a827458cab2d028de7e99736a419597f"
+    var apiURL =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiURL)
+    .then(function(response) {
+        response.json().then(function(data) {
+            displayWeather(data, city);
+        });
+    });
+};
+
+var displayWeather = function(weather, searchCity) {
+    weatherContainerElEl.textContent = "";
+    citySearchInputEl.textContent = searchCity;
+//date
+    var currentDate = document.createElement("span");
+    currentDate.textContent = " (" + dayjs(weather.dt.value).format("MMM D, YYYY") + ") ";
+    citySearchInputEl.appendChild(currentDate);
+
+    //image
+    var weatherIcon = document.createElement("img");
+    weatherIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + weather.weather[0].icon + ".png");
+    citySearchInputEl.appendChild(weatherIcon);
