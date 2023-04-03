b4b5272c1a12e6052d69d54aeda480e5c89ae7fd
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-04-03 08:43:13 -0500

    updated readme file

--- README.md
+++ README.md
@@ -7,15 +7,24 @@
 [Name](Link)
 
 ## Why I made this site
+I put together this site to show what we have learned up to this point in the bootcamp. Showing off HTML, CSS, JS along with many other 
+details to build a user and informational site for the end user.
 
 
 
 ## What I learned
+Inserting API's into JS can help the end user pull information from one site instead of bouncing around from site to site looking.
 
 
 
 ## Challenges 
+Getting the Open weather API to integrate into js was the biggest challenge.  My understanding of the concept is still very new 
+and very challenging for me to complete. Using Copilot to insert some the code to hopefully receive a working 5 day forecast. 
 
 
 
 ## Credits
+W3 schools  https://www.w3schools.com/w3css/w3css_cards.asp
+W3 schools  https://www.w3schools.com/w3css/w3css_cards.asp
+openweather https://openweathermap.org/api
+VScode Copilot https://docs.github.com/en/copilot/getting-started-with-github-copilot
--- assets/js/script.js
+++ assets/js/script.js
@@ -72,5 +72,53 @@ var displayWeather = function(weather, searchCity) {
 
     weatherContainerEl.appendChild(windSpeedEl);
 
+    var lat = weather.coord.lat;
+    var lon = weather.coord.lon;
+    getUvIndex(lat, lon);
+
 } 
 //console.log("hello")
+
+var getUvIndex = function(lat, lon) {
+    var apiKey ="a827458cab2d028de7e99736a419597f"
+    var apiURL =`https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`
+    fetch(apiURL)
+    .then(function(response) {
+        response.json().then(function(data) {
+            displayUvIndex(data);
+        });
+    });
+};
+
+var displayUvIndex = function(index) {
+    var uvIndexEl = document.createElement("div");
+    uvIndexEl.textContent = "UV Index: ";
+    uvIndexEl.classList = "list-group-item"
+
+    uvIndexValue = document.createElement("span");
+    uvIndexValue.textContent = index.value;
+
+    if (index.value <= 2) {
+        uvIndexValue.classList = "favorable"
+    }else if (index.value > 2 && index.value <= 8) {
+        uvIndexValue.classList = "moderate"
+    }else if (index.value > 8) {
+        uvIndexValue.classList = "severe"
+    }
+
+    uvIndexEl.appendChild(uvIndexValue);
+
+    weatherContainerEl.appendChild(uvIndexEl);
+};
+
+var get5day = function(city) {
+    var apiKey ="a827458cab2d028de7e99736a419597f"
+    var apiURL =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+    fetch(apiURL)
+    .then(function(response) {  
+        response.json().then(function(data) {
+            display5day(data);
+        });
+    });
+};
+
